883b673c48ec157b77d26be137fc870979cb332d
Pipo <pipao.pipo@gmail.com>
2022-08-16 11:56:20 -0300

    datos relevamiento

--- src/routes/relevamiento.js
+++ src/routes/relevamiento.js
@@ -13,39 +13,120 @@ router.post("/datos", async (req, res) => {
     let cdenuncia = 0
     let sdenuncia = 0
     let enproceso = 0
+
+    let uno = 0
+    let dos = 0
+    let tres = 0
+    console.log(datos[0]['Rango_Antiguedad'])
     for (var i = 0; i < datos.length; i++) {
-      
-       //console.log(datos[i]['Status'])
+     
+    
         switch (datos[i]['Status']) {
             case "Denuncia":
-                cdenuncia=+1
+                cdenuncia=cdenuncia+1
+
                 break;
             case "SinDenuncia":
-                sdenuncia+=1
+                sdenuncia=sdenuncia+1
                 break;
                 case "EnProceso":
-                    enproceso+=1
+                    enproceso=enproceso +1
                     break;
             default:
                 break;
         }
+        switch (datos[i]['Rango_Antiguedad']) {
+            case "0-4":
+                uno=uno+1
+
+                break;
+            case "4-8":
+                dos=dos+1
+                break;
+                case "8-12":
+                    tres=tres +1
+                    break;
+            default:
+                break;
+        }
+
+
         
     }
+            porcD =  (cdenuncia/datos.length*100).toFixed(2)
+          
+            porcSD =  (sdenuncia/datos.length*100).toFixed(2)
+            porcEP = (enproceso/datos.length*100).toFixed(2)
 
     const status = {
-        "cdenuncua": cdenuncia,
+        "familias":  datos.length,
+
+        "cdenuncia": cdenuncia,
+        "porcDenuncia":porcD,
+
         "SinDenuncia":sdenuncia,
-        "EnProceso": enproceso
+        "porcSDenuncia":porcSD,
+
+        "EnProceso": enproceso,
+        "porcEnProceso":porcEP
 
     }
-    const rta =[status]
-//    console.log(rta)
-    res.send(rta)
+
+    unoo = {
+        rango: "0-4",
+        cantidad: uno,
+    }
+    doss={
+        rango: "4-8",
+        cantidad: uno, 
+    }
+    tress={
+        rango: "8-12",
+        cantidad: tres,
+     }
+    
+     const rangoo =[unoo,doss,tress]
+  
+    const rta =[status,rangoo]
+    console.log(rta)
+    res.json(rta)
 
 
 })
 
 
+router.post("/cargar", async (req, res) => {
+    const { Zona, Material_Construccion,Status,Rango_Antiguedad,Observaciones } = req.body
+   const newDato ={
+    Zona, Material_Construccion,Status,Rango_Antiguedad,Observaciones 
+   }
+   
+   try {
+    await pool.query('INSERT INTO relevamiento SET ?', [newDato]);
+    res.send('Cargado correctamente!')
+   } catch (error) {
+    res.send('Algo Salio mal')
+   }
+  
+
+    
+
+})
+
+router.post("/borrardatoszona", async (req, res) => {
+    const { Zona} = req.body
+ 
+   try {
+    await pool.query('DELETE FROM relevamiento WHERE zona = ?', [Zona])
+    res.send('Borrado correctamente!')
+   } catch (error) {
+    res.send('Algo Salio mal')
+   }
+  
+
+    
+
+})
 
 
 
