be6c2cea6f21dc3216ed3c2727b71dd7ae3ed06c
Pipo <pipao.pipo@gmail.com>
2022-08-25 20:32:28 -0300

    lotes

--- src/routes/lotes.js
+++ src/routes/lotes.js
@@ -72,15 +72,16 @@ router.post('/calcularvalor', async (req, res) => {
     const nombre = 'Zona: '+ lote[0]['zona'] +' Manzana: '+lote[0]['manzana']  +' Parcela: '+lote[0]['parcela']
     finalSant= final*0.8
     const cuotas60 = finalSant/60
-    let puede = false
+    let puede = 'No puede adquirir el lote'
     if (ingresos>= cuotas60){
-        puede=true
+        puede='Puede adquirir el lote'
     }
     const detalle = {
         precio: final.toFixed(2),
         superficie: lote[0]['superficie'],
         nombre: nombre,
         cuotas60: cuotas60.toFixed(2),
+        ingresos: ingresos,
         puede:puede
     }
     console.log(detalle)
