9b0dcde83a86f1dfa2cf3e2257d9ff69bd09d0c8
Pipo <pipao.pipo@gmail.com>
2022-09-19 10:49:54 -0300

    subida key ignore

--- src/keys.js
+++ src/keys.js
@@ -4,8 +4,8 @@ module.exports = {
     database:{
         host: 'localhost',
         user: 'root',
-        password: '11235',
-       
+      //  password: '11235',
+        password: 'root',
         database: 'fideicomiso'
     }
 
