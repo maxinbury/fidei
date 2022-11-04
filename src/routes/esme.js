bb5ad2dcc067b85b75a14c4056c6273d788616f5
Pipo <pipao.pipo@gmail.com>
2022-11-03 22:39:35 -0300

    clases

--- src/routes/esme.js
+++ src/routes/esme.js
@@ -11,6 +11,15 @@ router.get('/listacursos', async (req, res) => {
 
 })
 
+router.get('/clases/:id', async (req, res) => {
+    const id = req.params.id
+    const lista = await pool.query('select * from esmeclases where id_curso = ?',[id])
+    const curso = await pool.query('select * from esmecursos where id = ?',[id])
+    res.json([lista,curso])
+
+})
+
+
 router.get('/expediente/:id', async (req, res) => {
 const id = req.params.id
     const exp = await pool.query('select * from expedientes where id= ?',[id])
@@ -40,6 +49,29 @@ router.post('/nuevocurso', async (req, res) => {
     
     })
 
+
+    router.post('/nuevaclase', async (req, res) => {
+        const {tema, fecha,otro }= req.body
+       try {
+        const newLink = {
+            nombre,
+            fecha,
+            otro
+        }
+        console.log(newLink)
+       await pool.query('insert esmeclases  set ?', newLink)
+    
+            
+        
+            res.send('todo ok ')
+       } catch (error) {
+        res.send(error)
+       }
+       
+       
+        
+        })
+    
 
 
 
