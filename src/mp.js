ccf5c583eb5cecc14c083376728511447f2f0c1b
Pipo <pipao.pipo@gmail.com>
2023-04-05 12:54:38 -0300

    mp

---
+++ src/mp.js
@@ -0,0 +1,10 @@
+module.exports = {
+    credenciales:{
+        
+            access_token: "APP_USR-6912288908238269-040415-5a368602ce75dc79949306b9fc9b4714-1345566063"
+          
+    }
+    
+
+}
+
