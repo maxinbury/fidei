af6a044a7538921a1828fe662af023127c3f7b62
FGEGitHub <pipao.pipo@gmail.com>
2023-12-29 11:58:26 -0300

    total lotes

--- src/controladores/lotesControlador.js
+++ src/controladores/lotesControlador.js
@@ -139,9 +139,9 @@ const listadeTodos = async (req, res) => {
  
     const lotes = await pool.query('select * from lotes left join (select cuil_cuit as cui, nombre from clientes) as sel on lotes.cuil_cuit=sel.cui ')
     const disponibles = await pool.query('select * from lotes where estado = "DISPONIBLE" or estado = "disponible"')
-    const parque = await pool.query('select * from lotes where (estado = "DISPONIBLE" or estado = "disponible") and zona ="PIT"')
+    const parque = await pool.query('select * from lotes where (estado = "DISPONIBLE" or estado = "disponible"or estado = "libre") and zona ="PIT"and manzana<>"Area verde" and manzana<>"Equipamiento del parque"')
     const parquetotal = await pool.query('select * from lotes where zona ="PIT"')
-    const parquevendidos = await pool.query('select * from lotes where (estado = "VENDIDO" or estado = "vendido") and zona ="PIT"')
+    const parquevendidos = await pool.query('select * from lotes where (estado = "VENDIDO" or estado = "vendido") and zona ="PIT" ')
     const ic3 = await pool.query('select * from lotes where (estado = "DISPONIBLE" or estado = "disponible") and zona ="IC3" ')
 
     console.log(disponibles.length)
